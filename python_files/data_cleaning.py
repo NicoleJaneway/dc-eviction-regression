bc42c33201390cc5c2357153ca40858db415cb55
Nicole Bills <bnicolebills@gmail.com>
2019-11-28 18:19:56 -0500

    add ft. selection

--- python_files/data_cleaning.py
+++ python_files/data_cleaning.py
@@ -53,8 +53,87 @@ def add_gis_ft2(super_df, sub_df, super_unit):
                 pass
     return sub_df
 
-def add_dev(ft_df, eviction_df):
+def create_master(ft_df, eviction_df):
     df = eviction_df.merge(ft_df, how='outer', on="GEOID", suffixes=('_Tract','_ft'))
     return df
-    
-                           
+
+def clean_master_puds(df):
+    df = df[['GEOID',
+'year',
+'name',
+'population',
+'poverty-rate',
+'renter-occupied-households',
+'pct-renter-occupied',
+'median-gross-rent',
+'median-household-income',
+'median-property-value',
+'rent-burden',
+'pct-white',
+'pct-af-am',
+'pct-hispanic',
+'pct-am-ind',
+'pct-asian',
+'pct-nh-pi',
+'pct-multiple',
+'pct-other',
+'eviction-filings',
+'evictions',
+'eviction-rate',
+'eviction-filing-rate',
+'low-flag',
+'imputed',
+'subbed',
+'OBJECTID',
+'TRACT',
+'FAGI_TOTAL_2010',
+'FAGI_MEDIAN_2010',
+'FAGI_TOTAL_2013',
+'FAGI_MEDIAN_2013',
+'FAGI_TOTAL_2011',
+'FAGI_MEDIAN_2011',
+'FAGI_TOTAL_2012',
+'FAGI_MEDIAN_2012',
+'FAGI_TOTAL_2014',
+'FAGI_MEDIAN_2014',
+'FAGI_TOTAL_2015',
+'FAGI_MEDIAN_2015',
+'geometry_Tract',
+'ward',
+'PUD_NAME',
+'PUD_WEB_URL',
+'PUD_CHANGE_NARRATIVE',
+'PUD_ZONING',
+'PUD_STATUS',
+'PUD_CASE_NUMBER',
+'GLOBALID',
+'WARD',
+'geometry_ft',
+'Zone_Cat',
+'ADDRESS',
+'PROJECT_NAME',
+'STATUS_PUBLIC',
+'AGENCY_CALCULATED',
+'AFFORDABLE_UNITS_AT_0_30_AMI',
+'AFFORDABLE_UNITS_AT_31_50_AMI',
+'AFFORDABLE_UNITS_AT_51_60_AMI',
+'AFFORDABLE_UNITS_AT_61_80_AMI',
+'AFFORDABLE_UNITS_AT_81_AMI',
+'TOTAL_AFFORDABLE_UNITS',
+'MAR_WARD',
+'Type',
+'ANC',
+'Total # Residential Units',
+'Total # Affordable Units',
+'Total # of 3 bedroom+ units (Fam sized units)',
+'Ownership (rental vs. condo or mix)',
+'Affordability notes (What levels of AMI% are avail)',
+'FULLADDRESS',
+'GIS_LAST_MOD_DTTM',
+'CASE_ID_update']]
+    return df
+
+def export_master(df, name):
+    df = df.astype(object)
+    df.to_csv(fr'../data/final_datasets/{name}.csv', index=False)
+    return "exported %s" %name
