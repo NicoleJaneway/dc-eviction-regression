7c2ca4bde45fbc32c6767f81bf9bb93c013685c7
Nicole Bills <bnicolebills@gmail.com>
2019-12-04 16:16:02 -0500

    add attempt to fit test data

--- python_files/regression.py
+++ python_files/regression.py
@@ -100,13 +100,15 @@ def multicolinearity_check(X):
     
     return plt.show(); 
 
-def big_lin_reg(X,y,X_labels):
+def lin_reg(X,y,X_labels=[]):
     """Fits linear regression model"""
     X = sm.add_constant(X)
     model = sm.OLS(y, X, hasconst=True )
     result = model.fit()
+    if X_labels == []:
+        X_labels = [el for el in range(len(X))]
     labels = ['intercept'] + X_labels
-    return result.summary(xname=labels)
+    return model, result, result.resid, result.model.exog, result.summary(xname=labels)
 
 # def residual_check1(X,y):
 #     sns.set_context('talk')
@@ -119,15 +121,10 @@ def big_lin_reg(X,y,X_labels):
 #         plt.subplots_adjust(wspace=.5, hspace=.5);
 #     return plt.show();
 
-def big_lin_reg_return_residuals(X,y):
-    X = sm.add_constant(X)
-    model = sm.OLS(y, X, hasconst=True )
-    result = model.fit()
-    return result.resid, result.model.exog
-
-    
 def residual_checks(X,y):
-    results, result_m_e = big_lin_reg_return_residuals(X,y)
+    X_labels = [el for el in range(X.shape[-1])]
+
+    model, results, result_m_e, summary = lin_reg(X,y,X_labels)
     
     name = ['Jarque-Bera', 'p-value', 'Skew', 'Kurtosis']
     test = sms.jarque_bera(results);
@@ -155,5 +152,55 @@ def residual_plot(X,y):
     ax.set_ylabel('Eviction Value', size=14);
     return plt.show();
 
-    
+import statsmodels.api as sm
+
+def stepwise_selection(X, y, 
+                       initial_list=[], 
+                       threshold_in=0.01, 
+                       threshold_out = 0.05, 
+                       verbose=True):
+    """ Perform a forward-backward feature selection 
+    based on p-value from statsmodels.api.OLS
+    Arguments:
+        X - pandas.DataFrame with candidate features
+        y - list-like with the target
+        initial_list - list of features to start with (column names of X)
+        threshold_in - include a feature if its p-value < threshold_in
+        threshold_out - exclude a feature if its p-value > threshold_out
+        verbose - whether to print the sequence of inclusions and exclusions
+    Returns: list of selected features 
+    Always set threshold_in < threshold_out to avoid infinite looping.
+    See https://en.wikipedia.org/wiki/Stepwise_regression for the details
+    """
+    included = list(initial_list)
+    while True:
+        changed=False
+        # forward step
+        excluded = list(set(X.columns)-set(included))
+        new_pval = pd.Series(index=excluded)
+        for new_column in excluded:
+            model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included+[new_column]]))).fit()
+            new_pval[new_column] = model.pvalues[new_column]
+        best_pval = new_pval.min()
+        if best_pval < threshold_in:
+            best_feature = new_pval.idxmin()
+            included.append(best_feature)
+            changed=True
+            if verbose:
+                print('Add  {:30} with p-value {:.6}'.format(best_feature, best_pval))
+
+        # backward step
+        model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included]))).fit()
+        # use all coefs except intercept
+        pvalues = model.pvalues.iloc[1:]
+        worst_pval = pvalues.max() # null if pvalues is empty
+        if worst_pval > threshold_out:
+            changed=True
+            worst_feature = pvalues.argmax()
+            included.remove(worst_feature)
+            if verbose:
+                print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
+        if not changed:
+            break
+    return included    
     
